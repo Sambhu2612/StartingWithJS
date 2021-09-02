c767ab3f7a2be263c29ea39ad1629b094efaffa1
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-02 19:28:55 +0530

    console

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Hey this is my first piece of javascript code !!!");
+
+console.log(8762347625462);
+
+console.log({name : "Piyush"})
+
+console.log(false, 8723648726, 'string', {name : "Piyush"})
+
+console.log("Piyush"*1000)
+
+console.warn("I am giving you warning")
+
+console.error("You are gone")
