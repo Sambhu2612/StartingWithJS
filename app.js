3f6be983864970971ce0ba4db6cf9af559690166
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-03 19:49:49 +0530

    Assignment operator

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-//   Arithmetic operators
+// Assignments operators
 
-const a = 2
-const b = 15
+let Name = "Piyush";
 
-console.log(a + b)
+let a = 10;
 
-console.log(a - b)
-
-console.log(a * b)
-
-console.log(a / b)
-
-console.log(a ** b)
-
-console.log(b % a)
-
-console.log('-------------------------------------------')
-
-let c = 5
-
-c *= 5
-
-// c operator=number is equivalent to c = c operator number
-// c = c * 5 = 5*5 = 25
-
-console.log(c)
-
-
-console.log(2+"2")
+a += Name;
+a /= 7;
+console.log(a);
