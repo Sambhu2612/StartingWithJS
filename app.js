6ea0dadb9eedf2fbdffddfb2dd7506f1596da88e
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-04 19:59:31 +0530

    toggle method

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const element = document.querySelector("#selectme");
-element.innerHTML = "This is from js";
-element.classList.add('selector')
-console.log(element.classList);
-element.classList.remove('selector');
-console.log(element.classList);
+const button = document.querySelector('button')
+const element = document.querySelector('#selectme')
 
-element.style.color = "blue";
 
-const body = document.querySelector('body');
-console.log(body.classList.toggle('dark'));
 
-console.log(document.querySelector('body').classList.contains('dark'));
+button.addEventListener('dblclick', () => {
+    element.classList.toggle('aqua');
+})
