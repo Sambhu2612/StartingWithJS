1d698639c67cf42d3293f626dbea8bad2d1c1bb7
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-06 19:28:24 +0530

    Switch Case Statement

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// const rock = document.querySelector('#rock')
-// const paper = document.querySelector('#paper')
-// const scissor = document.querySelector('#scissor')
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector("#board")
 
 
-function logName(Name = "John Doe", age = 18) {
-    // console.log("Nishta")
-    console.log(Name + " You are " + age)
+const handler = (e) => {
+    switch (e.target.id) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors";
+            break;
+        case 'paper':
+            board.innerHTML = "Paper traps Rock";
+            break;
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper";
+            break;
+        default:
+            board.innerHTML = "Click something"
+    }
+
 }
 
-const add = (a = 0, b = 0) => {
-    return a + b
-}
 
 
-console.log(add(12, 23))
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
