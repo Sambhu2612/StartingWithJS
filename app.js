7548cea509169dc3177d2d4643fc6347b1dd70fb
Sambhu2612 <piyush.mitra87@gmail.com>
2021-09-07 18:06:39 +0530

    Array methods

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
+const balls = Array.from(document.querySelectorAll('.ball'));
+const body = document.querySelector('body');
+balls.push("Hello");
+console.log(balls.toString());
+balls.unshift("Hi");
+balls.splice(1,2)
+console.log(balls.indexOf("Hi")+balls);
 
-for(let i=1;i<=10;i++){
+const arr = [1,2,3,4,5,6];
+arr.forEach((i) => {
     console.log(i);
-}
-
-const temp = () => {
-    var x = 5;
-}
-
-// console.log(x);
-
-
-let i = 1;
-while(i<=10){
-    console.log(i);
-    i++;
-}
+});
